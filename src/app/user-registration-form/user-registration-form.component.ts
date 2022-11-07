a58823286cd9f54ddcb3cb0bdbaf6ff8e5956463
htc <89924593+HienTrancong@users.noreply.github.com>
2022-11-07 21:15:38 +0100

    add Login form

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -16,7 +16,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  //the @Input decorator that defines the component’s input
+  //the @Input decorator that defines the component’s input, which bind to ngModel in html
   @Input() userData = { Username:'', Password:'', Email:'', Birthday:'' };
 
   constructor(
